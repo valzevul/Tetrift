42c3e4ae61f9b17f6021430fae17b7aac79a7969
Vadim Drobinin <vadim@drobinin.com>
2014-10-12 10:45:59 +0400

    Added Array2D class and background.

---
+++ Tetrift/Array2D.swift
@@ -0,0 +1,30 @@
+//
+//  Array2D.swift
+//  Tetrift
+//
+//  Created by Vadim Drobinin on 12/10/14.
+//  Copyright (c) 2014 Vadim Drobinin. All rights reserved.
+//
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+
+    var array: Array<T?>
+
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        
+        array = Array<T?>(count: rows * columns, repeatedValue: nil)
+    }
+
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[row * columns + column]
+            }
+        set(newValue) {
+            array[row * columns + column] = newValue
+        }
+    }
+}
